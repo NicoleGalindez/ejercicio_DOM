dd9c70b6e695a6cb84b7b0956e99c74134a3aefa
NicoleGalindez <129116184+NicoleGalindez@users.noreply.github.com>
2023-06-29 18:19:30 -0500

    de nuevo

--- cobo/js/nicole.js
+++ cobo/js/nicole.js
@@ -55,14 +55,46 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     
    
 
+
+//___ tutor 
+// let bienvenidotut= document.getElementById('bienvenida');
+// let textobienvenidotut=document.getElementById('textobienvenida');
+// let rutascomunestut=document.getElementById('rutascomunes');
+// let comotrabajatut=document.getElementById('comotrabaja');
+// let titcuadroverdetut= document.getElementById('titcuadroverde');    
+
+// //___ menor 
+// let bienvenidomen= document.getElementById('bienvenida');
+// let textobienvenidomen= document.getElementById('textobienvenida');
+// let titcuadroverdemen= document.getElementById('titcuadroverde');
+// let textcuadroverdemen= document.getElementById('textcuadroverde');
+// let rutascomunesmen= document.getElementById('rutascomunes');
+// let comotrabajamen= document.getElementById('comotrabaja');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Realiza la validación de usuario y contraseña
     if (username === "adso2556678" && password === "adso2023") {
       // Si el usuario y la contraseña son correctos, redirige a la página principal
-      window.location.href = "Home.html";
+      window.location.href = "home.html";
       alert("¡Formulario válido! Serás enviado a la lista de opciones para ingresar a ConfiApp.");
  
-
-
 
 
 
@@ -70,9 +102,42 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     
     else if (username === "tutor123" && password === "adso2023") {
         // Si el usuario y la contraseña son correctos, redirige a la página principal
-        window.location.href = "Home.html";
+        window.location.href = "home.html";
         alert("¡Formulario válido! Serás enviado a la lista de opciones para ingresar a ConfiApp.");
       
+
+     //   alert("Has ingresado como tutor");
+        //-----menu:---------------
+
+
+        //------contenido------------
+    //   bienvenidotut.innerText=`Bienvenido nuevamente a confiApp `;
+    //    textobienvenidotut.innerText=`¡Todos los días estamos trabajando duro para progresar, para que nuestra juventud esté llena de seguridad y confianza!`;
+    //    rutascomunestut.innerText=`Rutas más frecuentes `;
+    //    comotrabajatut.innerText=`¿Tienes dudas sobre como trabajamos?`;
+    //    titcuadroverdetut.innerText=`¿Deseas saber más sobre notsotros? `;
+      
+        //------imagenes------------
+    //    let imagen1tut=document.getElementById('imagenequipo1');
+    //    console.log(imagen1tut.src);
+    //    imagen1tut.src='assets/img/equipo.jpg'; // no la cambia
+
+
+    //    let imgRuta1tut=document.getElementById('imgRuta1');
+    //    console.log(imgRuta1tut.src);
+    //    imgRuta1.src='assets/img/puntosmapas.jpg';
+
+    //    let imgRuta2tut=document.getElementById('imgRuta2');
+    //    console.log(imgRuta2tut.src);
+    //    imgRuta2tut.src='assets/img/puntosmapas2.png';
+
+    //    let imgRuta3tut=document.getElementById('imgRuta3');
+    //    console.log(imgRuta3tut.src);
+    //    imgRuta3tut.src='assets/img/puntosmapas4.jpg';
+   
+
+
+
 
       }
 
@@ -81,9 +146,58 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
 
     else if (username === "menor123" && password === "adso2023") {
         // Si el usuario y la contraseña son correctos, redirige a la página principal
-        window.location.href = "Home.html";
+        window.location.href = "home.html";
         alert("¡Formulario válido! Serás enviado a la lista de opciones para ingresar a ConfiApp.");
        
+
+         //       alert('Has ingresado como menor');
+
+        //         //-----menu:---------------
+        // infopersonal.remove('Información personal');
+        // mimenor.remove('Mi menor');
+        // contactanos.remove('Contáctanos');  //.innerText=`Cerrar sesion`;
+
+        //     //------contenido------------
+        // bienvenidomen.innerText=`Bienvenido, sientete seguro con confiApp `;
+        // titcuadroverdemen.innerText=`¿Por qué confiar en nosotros ? `;
+        // textcuadroverdemen.innerText=`Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, enim officiis. Dicta, et!`;
+        // rutascomunesmen.innerText=`Mi rutas recorridas`;
+        // comotrabajamen.innerText=`¿Quieres saber como trabajamos para tu seguridad?`;
+
+        // // // NO SE QUITAAAA  
+        // regismenor.remove('Registrar menor');
+        // // numeros.remove=('1962 Familias');
+        // // numeros.remove=('5347 Rurtas guardadas');
+        // // numeros.remove=('87 Condecoraciones');
+        // // numeros.remove=('25 Ciudades');
+        //     //------imagenes------------
+
+
+
+        // let imagen1men=document.getElementById('imagenequipo1');
+        // console.log(imagen1men.src);
+        // imagen1men.src='assets/img/peopleworking.jpg';  // no la cambia
+
+
+
+
+        // let imgRuta1men=document.getElementById('imgRuta1');
+        // console.log(imgRuta1men.src);
+        // imgRuta1men.src='assets/img/puntosmapas.jpg';
+
+        // let imgRuta2men=document.getElementById('imgRuta2');
+        // console.log(imgRuta2men.src);
+        // imgRuta2men.src='assets/img/puntosmapas4.jpg';
+
+
+        // let imgRuta3men=document.getElementById('imgRuta3');
+        // console.log(imgRuta3men.src);
+        // imgRuta3men.src='assets/img/puntosmapas3.jpg';
+
+
+
+
+
       }
     else {
       // Si el usuario y la contraseña son incorrectos, redirige al modal de registro
